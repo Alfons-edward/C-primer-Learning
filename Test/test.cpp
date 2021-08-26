3b67041f59d44863dd9ecbbd8c659d8518d53849
zhangwei <meetzhangwei00@gmail.com>
2021-08-27 00:39:49 +0800

    "Test Git"

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
+    //Everyone studied the coding languge like to print the "hello world" first time 
     printf("hello world");
     return 0;
 }
